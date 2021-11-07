89f263b147cc1d6a3bbb9bc5d561661ae619e968
zX3no <dr_xeno@hotmail.com.au>
2021-11-07 19:56:24 +1030

    important: fancy constraint that doesnt work

--- gronk/src/ui.rs
+++ gronk/src/ui.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use tui::backend::Backend;
 use tui::layout::*;
 use tui::style::*;
@@ -121,6 +123,51 @@ pub fn draw_queue<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         &app.queue.ui_index,
     );
 
+    let widths = if !songs.is_empty() {
+        // let number: Vec<String> = songs
+        //     .into_iter()
+        //     .map(|song| song.number.to_string())
+        //     .collect();
+        let name: Vec<&String> = songs.into_iter().map(|song| &song.name).collect();
+        let album: Vec<&String> = songs.into_iter().map(|song| &song.album).collect();
+        let artist: Vec<&String> = songs.into_iter().map(|song| &song.artist).collect();
+
+        // let c1 = find_max_owned(&number);
+        let c1 = 4.0;
+        let c2 = find_max(&name) as f32;
+        let c3 = find_max(&album) as f32;
+        let c4 = find_max(&artist) as f32;
+        let total = c1 + c2 + c3 + c4;
+
+        let c1 = c1 / total * 100.0;
+        let c2 = c2 / total * 100.0;
+        let c3 = c3 / total * 100.0;
+        let c4 = c4 / total * 100.0;
+
+        let test = largest_remainder(&[c1, c2, c3, c4]);
+
+        let c1 = *test.get(0).unwrap() as u16;
+        let c2 = *test.get(1).unwrap() as u16;
+        let c3 = *test.get(2).unwrap() as u16;
+        let c4 = *test.get(3).unwrap() as u16;
+        // dbg!(c1, c2, c3, c4);
+        // panic!();
+
+        [
+            Constraint::Percentage(c1),
+            Constraint::Percentage(c2),
+            Constraint::Percentage(c3),
+            Constraint::Percentage(c4),
+        ]
+    } else {
+        [
+            Constraint::Percentage(25),
+            Constraint::Percentage(25),
+            Constraint::Percentage(25),
+            Constraint::Percentage(25),
+        ]
+    };
+
     let mut items: Vec<Row> = songs
         .iter()
         .map(|song| {
@@ -200,18 +247,63 @@ pub fn draw_queue<B: Backend>(f: &mut Frame<B>, app: &mut App) {
                 .borders(Borders::ALL)
                 .border_type(BorderType::Rounded),
         )
-        .widths(&[
-            Constraint::Percentage(8),
-            Constraint::Percentage(34),
-            Constraint::Percentage(32),
-            Constraint::Percentage(26),
-        ])
+        .widths(&widths)
         // ...and potentially show a symbol in front of the selection.
         .highlight_symbol("> ");
+
     //TODO: calculate longest length of track, album, artist name and change the constraints to fit
     //sometimes the track name is squished when it doesn't need too
 
     let mut state = TableState::default();
     state.select(*index);
     f.render_stateful_widget(t, area, &mut state);
+}
+fn find_max(items: &Vec<&String>) -> usize {
+    let mut i = 0;
+    for item in items {
+        if item.len() > i {
+            i = item.len();
+        }
+    }
+    i
+}
+fn find_max_owned(items: &Vec<String>) -> f32 {
+    let mut i = 0;
+    for item in items {
+        if item.len() > i {
+            i = item.len();
+        }
+    }
+    i as f32
+}
+fn largest_remainder(numbers: &[f32]) -> Vec<f32> {
+    //trunc | rem
+    let mut nums = Vec::new();
+    for (i, num) in numbers.iter().enumerate() {
+        nums.push((num.trunc(), num - (num / 1.0).trunc() * 1.0, i));
+    }
+
+    nums.sort_by(|(_, a, _), (_, b, _)| a.partial_cmp(b).unwrap());
+
+    let mut out: Vec<f32> = nums.iter().map(|(trunc, _, _)| trunc.clone()).collect();
+    while out.iter().sum::<f32>() != 100.0 {
+        let (trunc, rem, _) = nums.last().unwrap().clone();
+
+        let mut index = None;
+        let mut order = None;
+        for (i, (_, r, o)) in nums.iter().enumerate() {
+            if rem == *r {
+                index = Some(i);
+                order = Some(*o);
+            }
+        }
+        nums.remove(index.unwrap());
+        nums.insert(index.unwrap(), (trunc + 1.0, 0.0, order.unwrap()));
+
+        out = nums.iter().map(|(trunc, _, _)| trunc.clone()).collect();
+    }
+
+    nums.sort_by(|(_, _, a), (_, _, b)| a.partial_cmp(b).unwrap());
+
+    nums.into_iter().map(|(trunc, _, _)| trunc).collect()
 }
