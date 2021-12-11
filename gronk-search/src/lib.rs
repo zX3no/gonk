7a2d0e9e3e87568e179656cf48fd02cbf5c5533e
zX3no <dr_xeno@hotmail.com.au>
2021-12-12 01:34:13 +1030

    cleanup

---
+++ gronk-search/src/lib.rs
@@ -0,0 +1,91 @@
+use std::fmt::Display;
+
+use strsim::{levenshtein, sorensen_dice};
+
+pub struct SearchEngine {
+    data: Vec<SearchItem>,
+}
+
+impl SearchEngine {
+    pub fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+    pub fn insert(&mut self, item: SearchItem) {
+        self.data.push(item);
+    }
+    pub fn insert_vec(&mut self, items: Vec<SearchItem>) {
+        self.data.extend(items);
+    }
+    pub fn search(&self, query: &str) -> Vec<SearchItem> {
+        let mut results = Vec::new();
+        for item in &self.data {
+            let acc = sorensen_dice(&query.to_lowercase(), &item.name.to_lowercase());
+
+            //sometimes the algorithim is shit
+            //words like fortnite are favored over strings with the query????
+            //i guess we are using two algorithims
+            //only do this on single words otherwise bad things happen
+            let str = &item.name;
+            let mut skip = false;
+            let strs = str.split(' ').collect::<Vec<_>>();
+            if strs.len() <= 1 {
+                let l = levenshtein(&query.to_lowercase(), &str.to_lowercase());
+                if l > 5 {
+                    skip = true;
+                }
+            }
+
+            if !skip && acc > 0.0 {
+                results.push((item, acc))
+            }
+        }
+        results.sort_by(|(_, a), (_, b)| b.partial_cmp(&a).unwrap());
+        results.into_iter().map(|(item, _)| item.clone()).collect()
+    }
+}
+#[derive(Clone, Debug)]
+pub enum ItemType {
+    Song,
+    Album,
+    Artist,
+}
+
+#[derive(Clone, Debug)]
+pub struct SearchItem {
+    pub name: String,
+    pub song_id: Option<usize>,
+    pub album_artist: Option<String>,
+    pub item_type: ItemType,
+}
+
+impl SearchItem {
+    pub fn song(title: &str, id: usize) -> Self {
+        Self {
+            name: title.to_string(),
+            song_id: Some(id),
+            album_artist: None,
+            item_type: ItemType::Song,
+        }
+    }
+    pub fn album(name: &str, artist: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            song_id: None,
+            album_artist: Some(artist.to_string()),
+            item_type: ItemType::Album,
+        }
+    }
+    pub fn artist(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            song_id: None,
+            album_artist: None,
+            item_type: ItemType::Artist,
+        }
+    }
+}
+impl Display for SearchItem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
