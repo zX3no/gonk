20bfa769ea8839aa37a7d6466553fed02a201b32
zX3no <dr_xeno@hotmail.com.au>
2022-04-02 21:25:24 +1030

    proper panic handling

--- gonk/src/app.rs
+++ gonk/src/app.rs
@@ -63,8 +63,18 @@ pub struct App {
 
 impl App {
     pub fn new() -> Self {
-        let mut terminal = Terminal::new(CrosstermBackend::new(stdout())).unwrap();
+        //make sure the terminal recovers after a panic
+        let orig_hook = std::panic::take_hook();
+        std::panic::set_hook(Box::new(move |panic_info| {
+            let mut terminal = Terminal::new(CrosstermBackend::new(stdout())).unwrap();
+            disable_raw_mode().unwrap();
+            terminal.show_cursor().unwrap();
+            execute!(stdout(), LeaveAlternateScreen, DisableMouseCapture).unwrap();
+            orig_hook(panic_info);
+            std::process::exit(1);
+        }));
 
+        let mut terminal = Terminal::new(CrosstermBackend::new(stdout())).unwrap();
         execute!(stdout(), EnterAlternateScreen, EnableMouseCapture).unwrap();
         enable_raw_mode().unwrap();
         terminal.clear().unwrap();
@@ -305,15 +315,5 @@ impl App {
 }
 
 impl Drop for App {
-    fn drop(&mut self) {
-        disable_raw_mode().unwrap();
-        self.terminal.show_cursor().unwrap();
-
-        execute!(
-            self.terminal.backend_mut(),
-            LeaveAlternateScreen,
-            DisableMouseCapture
-        )
-        .unwrap();
-    }
+    fn drop(&mut self) {}
 }
--- gonk/src/main.rs
+++ gonk/src/main.rs
@@ -4,12 +4,6 @@ use std::io::Result;
 mod app;
 
 fn main() -> Result<()> {
-    let orig_hook = std::panic::take_hook();
-    std::panic::set_hook(Box::new(move |panic_info| {
-        orig_hook(panic_info);
-        std::process::exit(1);
-    }));
-
     let args: Vec<_> = std::env::args().skip(1).collect();
     if let Some(first) = args.first() {
         match first as &str {
