59f25ad8f7ed5f3895aad04043c0a76e496671c5
zX3no <28522740+zX3no@users.noreply.github.com>
2023-08-02 15:47:06 +0930

    Settings

--- gonk/src/settings.rs
+++ gonk/src/settings.rs
@@ -58,37 +58,23 @@ pub fn down(settings: &mut Settings) {
     }
 }
 
-//TODO: I liked the old item menu bold selections.
+//TODO: I liked the old item menu bold selections instead of white background.
 //It doesn't work on most terminals though :(
 pub fn draw<'a>(settings: &'a mut Settings, area: winter::Rect, buf: &mut winter::Buffer) {
     let mut items = Vec::new();
     for device in &settings.devices {
-        if device.name == settings.current_device {
-            items.push(vec![
-                text![">> ", fg(White).dim().bold()],
-                text![device.name.as_str(), fg(White).dim().bold()],
-            ]);
+        let item = if device.name == settings.current_device {
+            lines([text!(">> ", dim()), text!(&device.name)], None, None)
         } else {
-            // items.push(text!(format!("   {}", device.name)));
-            items.push(vec![text!(device.name.as_str())]);
-        }
+            lines([text!("   "), text!(&device.name)], None, None)
+        };
+        items.push(item);
     }
 
     if let Some(index) = settings.index {
-        if let Some(item) = items[index].get_mut(0) {
-            item.style = fg(Black).bg(White);
-        }
-        if let Some(item) = items[index].get_mut(1) {
-            item.style = fg(Black).bg(White);
-        }
-
-        // let item = items[index]
-        //     .clone()
-        //     .style(Style::default().fg(Color::Black).bg(Color::White));
-        // items[index] = item;
+        items[index].style = Some(fg(Black).bg(White));
     }
 
-    let items: Vec<_> = items.iter().map(|vec| lines(vec, None, None)).collect();
     let list = list(
         Some(block(
             Some("Output Device".into()),
@@ -99,22 +85,8 @@ pub fn draw<'a>(settings: &'a mut Settings, area: winter::Rect, buf: &mut winter
         )),
         &items,
         None,
-        style(),
+        None,
     );
 
     list.draw(area, buf, &mut list_state(settings.index));
-
-    // let list = List::new(&items)
-    //     .block(
-    //         Block::default()
-    //             .title("─Output Device")
-    //             .borders(Borders::ALL)
-    //             .border_type(BorderType::Rounded),
-    //     )
-    //     .highlight_style(Style::default());
-
-    // let mut state = ListState::default();
-    // state.select(settings.index);
-
-    // f.render_stateful_widget(list, area, &mut state);
 }
