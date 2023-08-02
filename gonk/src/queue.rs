9d162b5a566f54f88deaf9bb4b6ff4ecd6f9ac46
zX3no <28522740+zX3no@users.noreply.github.com>
2023-08-02 21:50:01 +0930

    Queue

--- gonk/src/queue.rs
+++ gonk/src/queue.rs
@@ -32,11 +32,16 @@ pub fn down(queue: &mut Queue) {
     queue.ui.down_with_len(queue.len);
 }
 
-pub fn draw(queue: &mut Queue, area: winter::Rect, buf: &mut winter::Buffer, mouse: Option<Event>) {
+pub fn draw(
+    queue: &mut Queue,
+    viewport: winter::Rect,
+    buf: &mut winter::Buffer,
+    mouse: Option<(u16, u16)>,
+) {
     let player = unsafe { queue.player.as_mut().unwrap() };
 
     let area = layout![
-        area,
+        viewport,
         Direction::Vertical,
         Constraint::Length(3),
         Constraint::Min(10),
@@ -165,18 +170,8 @@ pub fn draw(queue: &mut Queue, area: winter::Rect, buf: &mut winter::Buffer, mou
                             text!(song.artist.as_str(), bg(ARTIST).fg(Black))
                         ];
                     } else {
-                        // row![
-                        //     text!(">>", fg(White).dim().bold()),
-                        //     text!(song.track_number.to_string(), fg(NUMBER)),
-                        //     text!(song.title.as_str(), fg(TITLE)),
-                        //     text!(song.album.as_str(), fg(ALBUM)),
-                        //     text!(song.artist.as_str(), fg(ARTIST))
-                        // ]
                         rows[player_index].columns[0] = text!(">>", fg(White).dim().bold()).into();
                     };
-
-                    // items.remove(player_index);
-                    // items.insert(player_index, row);
 
                     //Current selection
                     if ui_index != player_index {
@@ -188,17 +183,6 @@ pub fn draw(queue: &mut Queue, area: winter::Rect, buf: &mut winter::Buffer, mou
                                 text!(song.album.as_str(), fg(Black).bg(ALBUM)),
                                 text!(song.artist.as_str(), fg(Black).bg(ARTIST))
                             ];
-                            // let row = Row::new(vec![
-                            //     Cell::default(),
-                            //     Cell::from(song.track_number.to_string())
-                            //         .style(Style::default().bg(NUMBER)),
-                            //     Cell::from(song.title.as_str()).style(Style::default().bg(TITLE)),
-                            //     Cell::from(song.album.as_str()).style(Style::default().bg(ALBUM)),
-                            //     Cell::from(song.artist.as_str()).style(Style::default().bg(ARTIST)),
-                            // ])
-                            // .style(Style::default().fg(Color::Black));
-                            // items.remove(ui_index);
-                            // items.insert(ui_index, row);
                         }
                     }
                 }
@@ -217,132 +201,106 @@ pub fn draw(queue: &mut Queue, area: winter::Rect, buf: &mut winter::Buffer, mou
             Borders::LEFT | Borders::RIGHT | Borders::BOTTOM,
             BorderType::Rounded,
         );
-        let header = header!["", "#", "Title", "Album", "Artist"];
+        //TODO: Header style.
+        let header = header![
+            "",
+            text!("#", bold()),
+            text!("Title", bold()),
+            text!("Album", bold()),
+            text!("Artist", bold())
+        ];
         let table = table(Some(header), Some(block), &con, rows, None, style());
         table.draw(area[1], buf, ui_index);
 
         let row_bounds = Some(table.get_row_bounds(ui_index, table.get_row_height(area[1])));
         row_bounds
-
-        // let t = Table::new(&items)
-        //     .header(
-        //         Row::new(["", "#", "Title", "Album", "Artist"])
-        //             .style(
-        //                 Style::default()
-        //                     .fg(Color::White)
-        //                     .add_modifier(Modifier::BOLD),
-        //             )
-        //             .bottom_margin(1),
-        //     )
-        //     .block(
-        //         Block::default()
-        //             .borders()
-        //             .border_type(BorderType::Rounded),
-        //     )
-        //     .widths(&con);
     };
 
-    // if log::last_message().is_none() {
-    //     //Seeker
-    //     let area = chunks[2];
-    //     if player.songs.is_empty() {
-    //         return f.render_widget(
-    //             Block::default()
-    //                 .border_type(BorderType::Rounded)
-    //                 .borders(Borders::BOTTOM | Borders::LEFT | Borders::RIGHT),
-    //             area,
-    //         );
-    //     }
+    if log::last_message().is_none() {
+        //Seeker
+        if player.songs.is_empty() {
+            return block(
+                None,
+                Borders::BOTTOM | Borders::LEFT | Borders::RIGHT,
+                BorderType::Rounded,
+            )
+            .draw(area[2], buf);
+        }
 
-    //     let elapsed = player.elapsed().as_secs_f64();
-    //     let duration = player.duration().as_secs_f64();
+        let elapsed = player.elapsed().as_secs_f64();
+        let duration = player.duration().as_secs_f64();
 
-    //     let seeker = format!(
-    //         "{:02}:{:02}/{:02}:{:02}",
-    //         (elapsed / 60.0).floor(),
-    //         (elapsed % 60.0) as u64,
-    //         (duration / 60.0).floor(),
-    //         (duration % 60.0) as u64,
-    //     );
+        let seeker = format!(
+            "{:02}:{:02}/{:02}:{:02}",
+            (elapsed / 60.0).floor(),
+            (elapsed % 60.0) as u64,
+            (duration / 60.0).floor(),
+            (duration % 60.0) as u64,
+        );
 
-    //     let ratio = elapsed.floor() / duration;
-    //     let ratio = if ratio.is_nan() {
-    //         0.0
-    //     } else {
-    //         ratio.clamp(0.0, 1.0)
-    //     };
+        let ratio = elapsed.floor() / duration;
+        let ratio = if ratio.is_nan() {
+            0.0
+        } else {
+            ratio.clamp(0.0, 1.0)
+        };
 
-    //     f.render_widget(
-    //         Gauge::default()
-    //             .block(
-    //                 Block::default()
-    //                     .border_type(BorderType::Rounded)
-    //                     .borders(Borders::ALL),
-    //             )
-    //             .gauge_style(Style::default().fg(SEEKER))
-    //             .ratio(ratio)
-    //             .label(seeker),
-    //         area,
-    //     );
-    // }
+        let block = block(None, Borders::ALL, BorderType::Rounded);
+        guage(Some(block), 0.5, seeker.into(), bg(SEEKER), style()).draw(area[2], buf);
+    }
 
-    // //Don't handle mouse input when the queue is empty.
-    // if player.songs.is_empty() {
-    //     return;
-    // }
+    //Don't handle mouse input when the queue is empty.
+    if player.songs.is_empty() {
+        return;
+    }
 
-    // //Handle mouse input.
-    // if let Some(event) = mouse_event {
-    //     let (x, y) = (event.column, event.row);
-    //     let header_height = 5;
+    //Handle mouse input.
+    if let Some((x, y)) = mouse {
+        let header_height = 5;
+        let size = viewport;
 
-    //     let size = f.size();
+        //Mouse support for the seek bar.
+        if (size.height - 3 == y || size.height - 2 == y || size.height - 1 == y)
+            && size.height > 15
+        {
+            let ratio = x as f32 / size.width as f32;
+            let duration = player.duration().as_secs_f32();
+            player.seek(duration * ratio);
+        }
 
-    //     //Mouse support for the seek bar.
-    //     if (size.height - 3 == y || size.height - 2 == y || size.height - 1 == y)
-    //         && size.height > 15
-    //     {
-    //         let ratio = x as f32 / size.width as f32;
-    //         let duration = player.duration().as_secs_f32();
-    //         player.seek(duration * ratio);
-    //     }
+        //Mouse support for the queue.
+        if let Some((start, _)) = row_bounds {
+            //Check if you clicked on the header.
+            if y >= header_height {
+                let index = (y - header_height) as usize + start;
 
-    //     //Mouse support for the queue.
-    //     if let Some((start, _)) = row_bounds {
-    //         //Check if you clicked on the header.
-    //         if y >= header_height {
-    //             let index = (y - header_height) as usize + start;
-
-    //             //Make sure you didn't click on the seek bar
-    //             //and that the song index exists.
-    //             if index < player.songs.len()
-    //                 && ((size.height < 15 && y < size.height.saturating_sub(1))
-    //                     || y < size.height.saturating_sub(3))
-    //             {
-    //                 queue.ui.select(Some(index));
-    //             }
-    //         }
-    //     }
-    // }
+                //Make sure you didn't click on the seek bar
+                //and that the song index exists.
+                if index < player.songs.len()
+                    && ((size.height < 15 && y < size.height.saturating_sub(1))
+                        || y < size.height.saturating_sub(3))
+                {
+                    queue.ui.select(Some(index));
+                }
+            }
+        }
+    }
 }
 
-// pub fn draw(queue: &mut Queue, f: &mut Frame, _: Rect, mouse_event: Option<MouseEvent>) {
-// }
+pub fn constraint(queue: &mut Queue, row: usize, shift: bool) {
+    if shift && queue.constraint[row] != 0 {
+        //Move row back.
+        queue.constraint[row + 1] += 1;
+        queue.constraint[row] = queue.constraint[row].saturating_sub(1);
+    } else if queue.constraint[row + 1] != 0 {
+        //Move row forward.
+        queue.constraint[row] += 1;
+        queue.constraint[row + 1] = queue.constraint[row + 1].saturating_sub(1);
+    }
 
-// pub fn constraint(queue: &mut Queue, row: usize, shift: bool) {
-//     if shift && queue.constraint[row] != 0 {
-//         //Move row back.
-//         queue.constraint[row + 1] += 1;
-//         queue.constraint[row] = queue.constraint[row].saturating_sub(1);
-//     } else if queue.constraint[row + 1] != 0 {
-//         //Move row forward.
-//         queue.constraint[row] += 1;
-//         queue.constraint[row + 1] = queue.constraint[row + 1].saturating_sub(1);
-//     }
-
-//     debug_assert!(
-//         queue.constraint.iter().sum::<u16>() == 100,
-//         "Constraint went out of bounds: {:?}",
-//         queue.constraint
-//     );
-// }
+    debug_assert!(
+        queue.constraint.iter().sum::<u16>() == 100,
+        "Constraint went out of bounds: {:?}",
+        queue.constraint
+    );
+}
