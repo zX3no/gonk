89b4fd6d15b3245400b21d04c4edd058461aff5b
zX3no <28522740+zX3no@users.noreply.github.com>
2023-09-29 15:20:41 +0930

    Testing ring buffer

--- gonk-player/src/main.rs
+++ gonk-player/src/main.rs
@@ -1,14 +1,6 @@
-use core::slice;
-use gonk_core::{profile, profiler};
-use gonk_player::decoder::Symphonia;
-use gonk_player::*;
-use std::{
-    collections::VecDeque,
-    ptr::{addr_of, addr_of_mut, null_mut},
-    sync::Arc,
-    thread,
-    time::Duration,
-};
+use gonk_core::*;
+use gonk_player::{decoder::Symphonia, *};
+use std::{ptr::addr_of_mut, thread, time::Duration};
 
 //I want to rework the player.
 
@@ -19,56 +11,57 @@ use std::{
 //I want a ring buffer that allows me to request somes bytes without re-allocating.
 
 fn main() {
-    let mut buffer: Rb<f32> = Rb::new(2);
-    let ptr = addr_of_mut!(buffer) as usize;
+    let orig_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        orig_hook(panic_info);
+        std::process::exit(1);
+    }));
+    // let mut buffer: Rb<f32, 1024> = Rb::new();
+    // let ptr = addr_of_mut!(buffer) as usize;
 
     // thread::spawn(move || {
-    //     let buffer = ptr as *mut Rb<f32>;
+    //     let buffer = ptr as *mut Rb<f32, 1024>;
     //     thread::sleep(Duration::from_millis(100));
     //     let _ = unsafe { (*buffer).pop_front() };
     //     // let _ = unsafe { (*buffer).pop_front() };
     // });
 
-    buffer.push_back(10.0);
-    buffer.push_back(11.0);
+    // for i in 0..1024 {
+    //     buffer.push_back(i as f32);
+    // }
+    // buffer.push_back(10.0 as f32);
+    // dbg!(buffer.buf.len());
+    // buffer.push_back(11.0);
+    // buffer.push_back(12.0);
     // buffer.push_back(12.0);
     // buffer.push_back(13.0);
 
-    profiler::print();
+    const N: usize = 4;
+    let mut rb: Rb<f32, N> = Rb::new();
+    let ptr = addr_of_mut!(rb) as usize;
 
-    // thread::park();
-    // buffer.push_back(10.0);
-    // dbg!(buffer.pop_front());
-    // loop {
-    //     dbg!(buffer.pop_front());
+    thread::spawn(move || {
+        let rb = unsafe { (ptr as *mut Rb<f32, N>).as_mut().unwrap() };
 
-    //     thread::sleep(Duration::from_millis(16));
-    // }
+        // thread::sleep(Duration::from_millis(2000));
+        let default = default_device();
+        let mut wasapi = Wasapi::new(&default, None).unwrap();
+        wasapi.fill(0.05, rb).unwrap();
+    });
 
-    // let default = default_device();
-    // let mut wasapi = Wasapi::new(&default, None).unwrap();
+    #[rustfmt::skip]
+    let mut sym = Symphonia::new(r"D:\Downloads\Variations for Winds_ Strings and Keyboards - San Francisco Symphony.flac").unwrap();
+    let mut elapsed = Duration::default();
+    let mut state = State::Playing;
 
-    // let buffer_size = wasapi.buffer_size().unwrap() as usize;
+    while let Some(packet) = sym.next_packet(&mut elapsed, &mut state) {
+        let samples = packet.samples();
 
-    // let rb: SpscRb<u8> = SpscRb::new(buffer_size * 8);
-    // let (prod, cons) = (rb.producer(), rb.consumer());
+        println!("Pushed samples {}", samples.len());
+        for sample in samples {
+            rb.push_back(*sample);
+        }
+    }
 
-    // thread::spawn(move || {
-    //     #[rustfmt::skip]
-    //     let mut sym = Symphonia::new(r"D:\Downloads\Variations for Winds_ Strings and Keyboards - San Francisco Symphony.flac").unwrap();
-    //     let mut elapsed = Duration::default();
-    //     let mut state = State::Playing;
-
-    //     while let Some(packet) = sym.next_packet(&mut elapsed, &mut state) {
-    //         let bytes = unsafe { packet.samples().align_to::<u8>().1 };
-    //         prod.write_blocking(bytes).unwrap();
-    //     }
-    // });
-
-    // let mut buf = vec![0; buffer_size];
-    // while let Some(_) = cons.read_blocking(&mut buf) {
-    //     wasapi.fill(0.05, &buf).unwrap();
-    // }
-
-    // thread::park();
+    thread::park();
 }
