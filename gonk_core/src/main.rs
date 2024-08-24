27446d8d06b3b9e11f8c6bda32440cc5786579db
Bay <28522740+zX3no@users.noreply.github.com>
2024-08-24 18:52:00 +0930

    Removed main from gonk_core

--- gonk_core/src/main.rs
+++
@@ -1,8 +0,0 @@
-use gonk_core::*;
-
-fn main() {
-    let song =
-        read_metadata(r"D:\OneDrive\Music\Joji\Chloe Burbank Vol. 1\01 Medicine (Remix).flac")
-            .unwrap();
-    dbg!(song);
-}
