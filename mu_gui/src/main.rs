f39e5cfdae10737f4aa7b80b4185d5f542c5b8e6
zfphex <28522740+zX3no@users.noreply.github.com>
2026-07-05 15:40:46 +0930

    bump version

--- mu_gui/src/main.rs
+++ mu_gui/src/main.rs
@@ -184,232 +184,227 @@ fn main() {
             }
         }
 
-        ui.start_frame(black());
-        if let Some(event) = ui.poll_event() {
-            match event {
-                Event::Quit | Event::Input(Key::Escape, _) => break,
-                _ => {}
+        ui.frame(|ui| {
+            let (top_nav_rect, body) = ui.split_v(30);
+            let (seekbar, body) = ui.split_rect_v(body, 24);
+            let (sidebar_rect, track_rect) = ui.split_rect_h(body, 260);
+
+            if let Some((menu, rect)) = current_menu {
+                let item_style = style()
+                    .width(180)
+                    .padlr(12)
+                    .padtb(8)
+                    .bg(rgb(35, 35, 35))
+                    .hover(rgb(60, 60, 60))
+                    .align(Alignment::Left)
+                    .depth(1);
+
+                ui.flow_skip(style().x(rect.x).y(top_nav_rect.height), Flow::Down, |ui| {
+                    for &item in dropdown_items(menu) {
+                        if ui.item(item, false, item_style).clicked {
+                            println!("{}", item);
+                            current_menu = None;
+                        }
+                    }
+                });
+
+                //TODO: This could maybe be part of the response.
+                if ui.lost_focus(rect) {
+                    current_menu = None;
+                }
             }
-        }
 
-        let (top_nav_rect, body) = ui.split_v(30);
-        let (seekbar, body) = body.split_v(24);
-        let (sidebar_rect, track_rect) = body.split_h(260);
+            ui.flow_right(bounds(top_nav_rect).bg(menu_bg), |ui| {
+                for (label, menu) in items {
+                    let state = ui.text(
+                        label,
+                        style()
+                            .height(top_nav_rect.height)
+                            .padl(14)
+                            .padr(14)
+                            .bg(menu_bg)
+                            .hover(menu_hover),
+                    );
 
-        if let Some((menu, rect)) = current_menu {
-            let item_style = style()
-                .width(180)
-                .padlr(12)
-                .padtb(8)
-                .bg(rgb(35, 35, 35))
-                .hover(rgb(60, 60, 60))
+                    if state.clicked {
+                        if current_menu.is_some_and(|(cm, _)| cm == menu) {
+                            current_menu = None;
+                        } else {
+                            current_menu = Some((menu, state.rect));
+                        }
+                    }
+                }
+
+                let bar = style().width(1).height(top_nav_rect.height).bg(bar_color);
+                ui.rect(bar);
+                ui.gap(120);
+                ui.rect(bar);
+                ui.gap(120);
+                ui.rect(bar);
+                ui.gap(120);
+                ui.gap(-214);
+
+                //Volume slider.
+                {
+                    let width = 200;
+                    let height = top_nav_rect.height;
+                    let rect = ui.walk_layout(width, height, 0).size;
+
+                    ui.paint_rect(rect, bg(rgb(25, 25, 25)));
+
+                    if let Some(percent) = ui.drag_percentage_x(rect) {
+                        volume = percent;
+                        //Don't go above 20% volume while testing :).
+                        let volume = (volume * 20.0) as u8;
+                        // eprintln!("Setting volume to {}", volume);
+                        player.set_volume(volume);
+                    }
+
+                    let track_height = 6;
+                    let cy = rect.y + height / 2;
+
+                    ui.paint_triangle(
+                        (rect.x, cy + track_height),
+                        (rect.x + width, cy + track_height),
+                        (rect.x + width, cy.saturating_sub(track_height)),
+                        bg(black()),
+                    );
+
+                    let thumb_w = 12;
+                    let thumb_h = 18;
+                    let available_width = width.saturating_sub(thumb_w);
+                    let thumb_x = rect.x + (volume * available_width as f32).round() as usize;
+                    let thumb_y = rect.y + (height.saturating_sub(thumb_h)) / 2;
+                    let thumb_color = rgb(0, 102, 204);
+
+                    ui.paint_rect(
+                        Rect::new(thumb_x, thumb_y, thumb_w, thumb_h),
+                        bg(thumb_color),
+                    );
+                }
+            });
+
+            //Seekbar
+            {
+                ui.paint_rect(seekbar, bg(menu_bg));
+                let inner = seekbar.inner(4, 6);
+                ui.paint_rect(inner, bg(menu_bg).border(border_color));
+
+                //Update during playback.
+                let duration = player.duration().as_secs_f32();
+                let elapsed = player.elapsed().as_secs_f32();
+                let ratio = (elapsed.floor() / duration).clamp(0.0, 1.0);
+                //TODO: This causes flicker since the player doesn't seek instantly :(
+                seekbar_ratio = if ratio.is_nan() { 0.0 } else { ratio };
+
+                //Only seek on release
+                if ui.clicked(seekbar) {
+                    if let Some((x, _)) = ui.window.mouse_pos() {
+                        let x = (x as usize).saturating_sub(inner.x);
+                        let ratio = (x as f32 / inner.width as f32).clamp(0.0, 1.0);
+                        let pos = duration * ratio;
+                        player.seek_to(Duration::from_secs_f32(pos));
+                    }
+                }
+
+                //Update the scrollbar visually but don't update the player.
+                if let Some(ratio) = ui.drag_percentage_x(inner) {
+                    seekbar_ratio = ratio;
+                }
+
+                let x = inner.width as f32 * seekbar_ratio;
+                let (w, h) = (11, 4);
+                ui.paint_rect(
+                    Rect::new(x as usize, seekbar.y + h / 2, w, seekbar.height.saturating_sub(h)),
+                    bg(accent_blue),
+                );
+            }
+
+            let row_style = style()
+                .pad(8)
+                .padl(12)
+                .hover(rgb(35, 35, 35))
+                .fill_width()
+                .hover_border(rgb(90, 90, 90))
+                .selected(rgb(82, 82, 82))
+                .padl(12)
                 .align(Alignment::Left)
-                .depth(1);
+                .selected_border(rgb(170, 170, 170));
 
-            ui.flow_once(style().x(rect.x).y(top_nav_rect.height), Flow::Down, |ui| {
-                for &item in dropdown_items(menu) {
-                    if ui.item(item, false, item_style).clicked {
-                        println!("{}", item);
-                        current_menu = None;
+            ui.scroll_view(
+                bounds(sidebar_rect).bg(panel_bg),
+                &mut browser_scroll,
+                |ui| {
+                    ui.text("All Music", style().fg(text_dim).pad(6));
+
+                    for artist in &artists {
+                        if ui.item(*artist, false, row_style).clicked {
+                            selected_artist = artist;
+                            playlist.clear();
+
+                            let albums = db.albums_by_artist(artist);
+
+                            for album in albums {
+                                playlist.extend(album.songs.clone());
+                            }
+
+                            if !playlist.is_empty() {
+                                playlist.select(Some(1));
+                            }
+                        }
+                    }
+
+                    ui.paint_rect(
+                        sidebar_rect,
+                        style().border(border_color).border_side(RIGHT),
+                    );
+                },
+            );
+
+            //This is kinda cursed.
+            let (track_rect, scrollbar) = ui.split_rect_h(track_rect, Size::FillMinus(20));
+
+            let state = ui.scroll_view(track_rect, &mut track_scroll, |ui| {
+                ui.text(
+                    selected_artist,
+                    style()
+                        .fg(accent_blue)
+                        .font_size(14)
+                        .padl(8)
+                        .padb(4)
+                        .height(24),
+                );
+
+                for (idx, track) in playlist.iter().enumerate() {
+                    let label = format!("{}. {}", track.track_number, track.title);
+                    if ui.item(label, idx == selected_song, row_style).clicked {
+                        selected_song = idx;
+                        play(&mut player, track, true);
+                        // player.play_song(track.path, track.gain, true);
                     }
                 }
             });
 
-            //TODO: This could maybe be part of the response.
-            if ui.lost_focus(rect) {
-                current_menu = None;
-            }
-        }
-
-        ui.flow_right(bounds(top_nav_rect).bg(menu_bg), |ui| {
-            for (label, menu) in items {
-                let state = ui.text(
-                    label,
-                    style()
-                        .height(top_nav_rect.height)
-                        .padl(14)
-                        .padr(14)
-                        .bg(menu_bg)
-                        .hover(menu_hover),
-                );
-
-                if state.clicked {
-                    if current_menu.is_some_and(|(cm, _)| cm == menu) {
-                        current_menu = None;
-                    } else {
-                        current_menu = Some((menu, state.rect));
-                    }
-                }
-            }
-
-            let bar = style().width(1).height(top_nav_rect.height).bg(bar_color);
-            ui.rect(bar);
-            ui.gap(120);
-            ui.rect(bar);
-            ui.gap(120);
-            ui.rect(bar);
-            ui.gap(120);
-            ui.gap(-214);
-
-            //Volume slider.
             {
-                let width = 200;
-                let height = top_nav_rect.height;
-                let rect = ui.walk_layout(width, height, 0).size;
+                let s = scrollbar.inner(4, 0);
+                let (y, h) = (s.y as f32, s.height as f32);
+                let thumb_h = 80.0;
+                // Calculate the exact space the bar can move within.
+                let available_height = (h - thumb_h).max(0.0);
+                let mut ratio = (track_scroll as f32 / state.max_scroll as f32).clamp(0.0, 1.0);
 
-                ui.paint_rect(rect, bg(rgb(25, 25, 25)));
-
-                if let Some(percent) = ui.drag_percentage_x(rect) {
-                    volume = percent;
-                    //Don't go above 20% volume while testing :).
-                    let volume = (volume * 20.0) as u8;
-                    // eprintln!("Setting volume to {}", volume);
-                    player.set_volume(volume);
+                if ui.dragged(scrollbar) {
+                    // Offset the mouse position by half the bar height so the drag centers on the thumb.
+                    let mousey = ui.mouse_position().y as f32 - y - (thumb_h / 2.0);
+                    ratio = (mousey / available_height).clamp(0.0, 1.0);
+                    track_scroll = (ratio * state.max_scroll as f32).round() as usize;
                 }
 
-                let track_height = 6;
-                let cy = rect.y + height / 2;
-
-                ui.paint_triangle(
-                    (rect.x, cy + track_height),
-                    (rect.x + width, cy + track_height),
-                    (rect.x + width, cy.saturating_sub(track_height)),
-                    bg(black()),
-                );
-
-                let thumb_w = 12;
-                let thumb_h = 18;
-                let available_width = width.saturating_sub(thumb_w);
-                let thumb_x = rect.x + (volume * available_width as f32).round() as usize;
-                let thumb_y = rect.y + (height.saturating_sub(thumb_h)) / 2;
-                let thumb_color = rgb(0, 102, 204);
-
-                ui.paint_rect(
-                    Rect::new(thumb_x, thumb_y, thumb_w, thumb_h),
-                    bg(thumb_color),
-                );
+                let y = s.y + (ratio * available_height).round() as usize;
+                let thumb = Rect::new(s.x, y, s.width, thumb_h as usize);
+                ui.paint_rect(thumb, bg(rgb(80, 80, 80)));
             }
         });
 
-        //Seekbar
-        {
-            ui.paint_rect(seekbar, bg(menu_bg));
-            let inner = seekbar.inner(4, 6);
-            ui.paint_rect(inner, bg(menu_bg).border(border_color));
-
-            //Update during playback.
-            let duration = player.duration().as_secs_f32();
-            let elapsed = player.elapsed().as_secs_f32();
-            let ratio = (elapsed.floor() / duration).clamp(0.0, 1.0);
-            //TODO: This causes flicker since the player doesn't seek instantly :(
-            seekbar_ratio = if ratio.is_nan() { 0.0 } else { ratio };
-
-            //Only seek on release
-            if ui.clicked(seekbar) {
-                let x = ui.window.mouse_position.x.saturating_sub(inner.x);
-                let ratio = (x as f32 / inner.width as f32).clamp(0.0, 1.0);
-                let pos = duration * ratio;
-                player.seek_to(Duration::from_secs_f32(pos));
-            }
-
-            //Update the scrollbar visually but don't update the player.
-            if let Some(ratio) = ui.drag_percentage_x(inner) {
-                seekbar_ratio = ratio;
-            }
-
-            let x = inner.width as f32 * seekbar_ratio;
-            let (w, h) = (11, 4);
-            ui.paint_rect(
-                Rect::new(x as usize, seekbar.y + h / 2, w, seekbar.height - h),
-                bg(accent_blue),
-            );
-        }
-
-        let row_style = style()
-            .pad(8)
-            .padl(12)
-            .hover(rgb(35, 35, 35))
-            .fill_width()
-            .hover_border(rgb(90, 90, 90))
-            .selected(rgb(82, 82, 82))
-            .padl(12)
-            .align(Alignment::Left)
-            .selected_border(rgb(170, 170, 170));
-
-        ui.scroll_view(
-            bounds(sidebar_rect).bg(panel_bg),
-            &mut browser_scroll,
-            |ui| {
-                ui.text("All Music", style().fg(text_dim).pad(6));
-
-                for artist in &artists {
-                    if ui.item(*artist, false, row_style).clicked {
-                        selected_artist = artist;
-                        playlist.clear();
-
-                        let albums = db.albums_by_artist(artist);
-
-                        for album in albums {
-                            playlist.extend(album.songs.clone());
-                        }
-
-                        if !playlist.is_empty() {
-                            playlist.select(Some(1));
-                        }
-                    }
-                }
-
-                ui.paint_rect(
-                    sidebar_rect,
-                    style().border(border_color).border_side(RIGHT),
-                );
-            },
-        );
-
-        //This is kinda cursed.
-        let (track_rect, scrollbar) = ui.split_rect_h(track_rect, Size::FillMinus(20));
-
-        let state = ui.scroll_view(track_rect, &mut track_scroll, |ui| {
-            ui.text(
-                selected_artist,
-                style()
-                    .fg(accent_blue)
-                    .font_size(14)
-                    .padl(8)
-                    .padb(4)
-                    .height(24),
-            );
-
-            for (idx, track) in playlist.iter().enumerate() {
-                let label = format!("{}. {}", track.track_number, track.title);
-                if ui.item(label, idx == selected_song, row_style).clicked {
-                    selected_song = idx;
-                    play(&mut player, track, true);
-                    // player.play_song(track.path, track.gain, true);
-                }
-            }
-        });
-
-        {
-            let s = scrollbar.inner(4, 0);
-            let (y, h) = (s.y as f32, s.height as f32);
-            let thumb_h = 80.0;
-            // Calculate the exact space the bar can move within.
-            let available_height = (h - thumb_h).max(0.0);
-            let mut ratio = (track_scroll as f32 / state.max_scroll as f32).clamp(0.0, 1.0);
-
-            if ui.dragged(scrollbar) {
-                // Offset the mouse position by half the bar height so the drag centers on the thumb.
-                let mousey = ui.mouse_position().y as f32 - y - (thumb_h / 2.0);
-                ratio = (mousey / available_height).clamp(0.0, 1.0);
-                track_scroll = (ratio * state.max_scroll as f32).round() as usize;
-            }
-
-            let y = s.y + (ratio * available_height).round() as usize;
-            let thumb = Rect::new(s.x, y, s.width, thumb_h as usize);
-            ui.paint_rect(thumb, bg(rgb(80, 80, 80)));
-        }
-
-        ui.draw_frame();
     }
 }
